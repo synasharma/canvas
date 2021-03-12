a44786b246ecc431b85d1a5bba338d4526b00555
synasharma <61623971+synasharma@users.noreply.github.com>
2021-03-12 12:03:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas=createCanvas(500,400);
+    canvas.center();
+}
+
+function draw()
+{
+
+}
+
+function take_snap()
+{
+    save("lipspic.png");
+}
